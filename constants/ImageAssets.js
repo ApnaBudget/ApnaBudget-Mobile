d86e16b14bbc99fe68b6f6aecde4e5ee0b4b17c3
Samay Singh <ssamaysinght@gmail.com>
2024-05-08 19:31:37 +0530

    feat: added a otp and forget password screen and some minor changes in styling

--- constants/ImageAssets.js
+++ constants/ImageAssets.js
@@ -1,9 +1,13 @@
 export const ImagesAssets = {
-    appLogo: require('../assets/images/logo.png'),
-    welcomeBackground: require('../assets/images/welcome-bg.png'),
-    demoGraph: require('../assets/images/demo-graph.png'),
-    welcomeBackground: require("../assets/images/welcome-bg.png"),
-    googleLogo: require("../assets/images/google-logo.png"),
-    appleLogo: require("../assets/images/apple-logo.png"),
-    authBackground : require("../assets/images/authBackground.png"),
-}
+  appLogo: require("../assets/images/logo.png"),
+  welcomeBackground: require("../assets/images/welcome-bg.png"),
+  demoGraph: require("../assets/images/demo-graph.png"),
+  welcomeBackground: require("../assets/images/welcome-bg.png"),
+  googleLogo: require("../assets/images/google-logo.png"),
+  appleLogo: require("../assets/images/apple-logo.png"),
+  authBackground: require("../assets/images/authBackground.png"),
+  verified: require("../assets/images/verified.png"),
+  reset: require("../assets/images/reset.png"),
+  msg: require("../assets/images/msg.png"),
+  mail: require("../assets/images/mail.png"),
+};
